07139ffb9e61a8b25eecb2a8f5318736596a4b70
Victor Prouff <contact@victorprouff.fr>
2025-02-07 20:28:02 +0100

    Ok

--- VTools/Models/EnVrac/Article.cs
+++ VTools/Models/EnVrac/Article.cs
@@ -2,7 +2,7 @@ namespace VTools.Models.EnVrac;
 
 public class Article
 {
-    private Article(string? title, string? description, string[] urls, Category category)
+    private Article(string? title, string? description, Link[] urls, Category category)
     {
         Title = title;
         Description = description;
@@ -12,11 +12,15 @@ public class Article
 
     public string? Title { get; private set; }
     public string? Description { get; private set; }
-    public string[] Urls { get; private set; }
+    public Link[] Urls { get; private set; }
     public Category Category { get; private set; }
 
-    public static Article Create(string? title, string? description, string[] urls, Category category) =>
+    public static Article Create(string? title, string? description, Link[] urls, Category category) =>
         new(title, description, urls, category);
 
 }
 
+public record Link(string Url, string Title = "Source")
+{
+    public string ToMarkdown() => $"[{Title}]({Url})";
+}
